ab731a7dab3148e017949f3018542b06091f5115
JamiYashwanth <jamiyashwanth18@gmail.com>
2021-07-21 11:09:22 +0530

    Added light theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router,Route } from "react-router-dom"
+import Homepage from "./Components/Homepage/Homepage";
+import Info from "./Components/Info/Info";
+import Navbar from "./Components/Navbar/Navbar";
+import {useState} from 'react'
+import {ThemeProvider , createGlobalStyle} from "styled-components";
+import './App.css'
+
+const GlobalStyle = createGlobalStyle`
+body{
+  background : ${props => props.theme.mode === 'dark' ? "linear-gradient(to right bottom,#03071E,#001524)" : 'linear-gradient(to right bottom,#F0EFEB,#FFFCF2)'};
+  background-attachment : fixed;
+  color : ${props => props.theme.mode === 'dark' ? "#fff" : "#0d3b66"};
+}
+`
+
 
 function App() {
+  const [theme,setTheme] = useState('dark');
+
+  const ToggleTheme = () => {
+   (theme === 'dark') ? setTheme('light') : setTheme('dark'); 
+  }
+
   return (
+    <ThemeProvider theme={{mode : theme}}>
+      <>
+      <GlobalStyle />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar theme={theme} ToggleTheme={ToggleTheme}/>
+      <Route path='/' exact render={() => <Homepage theme={theme} />} />
+      <Route path='/api/all' render={() => <Info theme={theme} />} />
+      <Route path ='/api/codeforces' render={() => <Info theme={theme} />} />       
+      <Route path ='/api/codechef' render={() => <Info theme={theme} />} />
+      <Route path ='/api/atcoder' render={() => <Info theme={theme} />} />
+      <Route path ='/api/leetcode' render={() => <Info theme={theme} />} />
+      <Route path ='/api/hackerearth' render={() => <Info theme={theme} />} />
+      <Route path ='/api/hackerrank' render={() => <Info theme={theme} />} />
+      <Route path ='/api/google' render={() => <Info theme={theme} />} />
+      <Route path ='/api/topcoder' render={() => <Info theme={theme} />} />
+      </Router>
     </div>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,150 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import "./navbar.css";
+
+const NavLink = styled(Link)`
+  color: ${(props) => (props.theme === "dark" ? "#fff" : "#0d3b66")};
+  text-decoration: none;
+`;
+
+const CpSpan = styled.span`
+  color: ${(props) => (props.theme === "dark" ? "#48cae4" : "#ef476f")};
+`;
+
+export default function Navbar(props) {
+  const [toggle, setToggle] = useState(false);
+  const theme = props.theme;
+  return (
+    <>
+      <div className="navbar">
+        <div className="title">
+          <NavLink to="/" theme={theme}>
+            <CpSpan theme={theme}>CP</CpSpan>
+            CALENDAR
+          </NavLink>
+        </div>
+        <div className="list">
+          <li className="list-item">
+            <NavLink to="/api/all" theme={theme}>
+              ALL
+            </NavLink>
+          </li>
+          <li className="list-item">
+            <NavLink to="/api/codeforces" theme={theme}>
+              CODEFORCES
+            </NavLink>
+          </li>
+          <li className="list-item">
+            <NavLink to="/api/codechef" theme={theme}>
+              CODECHEF
+            </NavLink>
+          </li>
+          <li className="list-item">
+            <NavLink to="/api/leetcode" theme={theme}>
+              LEETCODE
+            </NavLink>
+          </li>
+          <li className="list-item">
+            <NavLink to="/api/atcoder" theme={theme}>
+              ATCODER
+            </NavLink>
+          </li>
+          <li className="list-item">
+            <NavLink to="/api/hackerearth" theme={theme}>
+              HACKEREARTH
+            </NavLink>
+          </li>
+          <li className="list-item">
+            <NavLink to="/api/hackerrank" theme={theme}>
+              HACKERRANK
+            </NavLink>
+          </li>
+          <li className="list-item">
+            <NavLink to="/api/google" theme={theme}>
+              GOOGLE
+            </NavLink>
+          </li>
+          <li className="list-item">
+            <NavLink to="/api/topcoder" theme={theme}>
+              TOPCODER
+            </NavLink>
+          </li>
+        </div>
+        {theme === "dark" ? (
+          <i
+            onClick={props.ToggleTheme}
+            className="fas fa-adjust"
+            style={{ cursor: "pointer" }}
+          />
+        ) : (
+          <i
+            onClick={props.ToggleTheme}
+            className="far fa-moon"
+            style={{ cursor: "pointer" }}
+          />
+        )}
+        {!toggle ? (
+          <i
+            onClick={() => setToggle(!toggle)}
+            className="bars fas fa-bars"
+          ></i>
+        ) : (
+          <i
+            onClick={() => setToggle(!toggle)}
+            className="bars fas fa-times"
+          ></i>
+        )}
+      </div>
+      {toggle ? (
+        <div className="respNav">
+          <li onClick={() => setToggle(false)} className="list-item">
+            <NavLink to="/api/all" theme={theme}>
+              ALL
+            </NavLink>
+          </li>
+          <li onClick={() => setToggle(false)} className="list-item">
+            <NavLink to="/api/codeforces" theme={theme}>
+              CODEFORCES
+            </NavLink>
+          </li>
+          <li onClick={() => setToggle(false)} className="list-item">
+            <NavLink to="/api/codechef" theme={theme}>
+              CODECHEF
+            </NavLink>
+          </li>
+          <li onClick={() => setToggle(false)} className="list-item">
+            <NavLink to="/api/leetcode" theme={theme}>
+              LEETCODE
+            </NavLink>
+          </li>
+          <li onClick={() => setToggle(false)} className="list-item">
+            <NavLink to="/api/atcoder" theme={theme}>
+              ATCODER
+            </NavLink>
+          </li>
+          <li onClick={() => setToggle(false)} className="list-item">
+            <NavLink to="/api/hackerearth" theme={theme}>
+              HACKEREARTH
+            </NavLink>
+          </li>
+          <li onClick={() => setToggle(false)} className="list-item">
+            <NavLink to="/api/hackerrank" theme={theme}>
+              HACKERRANK
+            </NavLink>
+          </li>
+          <li onClick={() => setToggle(false)} className="list-item">
+            <NavLink to="/api/google" theme={theme}>
+              GOOGLE
+            </NavLink>
+          </li>
+          <li onClick={() => setToggle(false)} className="list-item">
+            <NavLink to="/api/topcoder" theme={theme}>
+              TOPCODER
+            </NavLink>
+          </li>
+        </div>
+      ) : null}
+    </>
+  );
+}
